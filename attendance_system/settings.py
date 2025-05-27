e7df4f5087f77b0460d8b2bf0631a06d94a8b163
AkiraArkangel <jaymarcortez.141997@gmail.com>
2025-05-28 00:54:37 +0800

    Update settings.py

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -83,11 +83,11 @@ WSGI_APPLICATION = 'attendance_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL')) {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    )
 }
+
 
 
 # Password validation
