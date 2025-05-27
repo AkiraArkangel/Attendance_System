e40c4896b60338afa74215513748b1d6b008cf32
AkiraArkangel <jaymarcortez.141997@gmail.com>
2025-05-27 23:58:55 +0800

    Update settings.py

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gpbu&eyy6pmimg5_poa+j+ieq(vv*c+^le5#p%k*hs8e5+il%e'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
